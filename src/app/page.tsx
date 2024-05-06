162ffbf8073b5a62be4c7ad4de501455e9fbbef3
codingwebsa <codingwebsa@gmail.com>
2024-05-06 20:47:20 +0600

    optimize: speed
    by removing the content from every post object

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { allPosts } from "contentlayer/generated";
+import { type Post, allPosts } from "contentlayer/generated";
 import { compareDesc } from "date-fns";
 
 import "~/styles/mdx.css";
@@ -7,6 +7,16 @@ import Navigation from "./_components/navigation";
 export default async function Home() {
   const posts = allPosts
     .filter((post) => post.published)
+    .map((obj) => ({
+      _id: obj._id,
+      published: obj.published,
+      slugAsParams: obj.slugAsParams,
+      slug: obj.slug,
+      title: obj.title,
+      date: obj.date,
+      tags: obj.tags,
+      type: obj.type,
+    }))
     .sort((a, b) => {
       return compareDesc(new Date(a.date), new Date(b.date));
     });
@@ -14,7 +24,7 @@ export default async function Home() {
   return (
     <main className="mx-auto max-w-2xl">
       <div className="pt-28">
-        <Navigation posts={posts} />
+        <Navigation posts={posts as Post[]} />
       </div>
     </main>
   );
